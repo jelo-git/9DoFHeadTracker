bfdc193ce3106188acae926a7b2c1a447edd14b0
jelo-git <batogfilip@gmail.com>
2023-11-02 22:50:53 +0100

    commit

--- testSoftware/routes/index.js
+++ testSoftware/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const { SerialPort } = require('serialport');
 
 //set up the routes
 router.get('/', (req, res) => {
-    res.render('index', { title: '9 Degrees of Freedom Sandbox' });
+    res.render('index', { title: '9 Degrees of Freedom Sandbox', ports: ports });
+});
+
+router.post('/serials', (req, res) => {
+    SerialPort.list().then((ports) => {
+        res.json(ports);
+    });
 });
 
 module.exports = router;
